f32f7e29aae28730d67bd6b8165fba983a5f8d53
Sloian <andriysolonynka10@gmail.com>
2023-01-04 11:46:32 +0200

    initProject

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '6d362fa562mshbec5528b8b61a2dp1dac25jsn4fa71d2db947',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '6d362fa562mshbec5528b8b61a2dp1dac25jsn4fa71d2db947',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
